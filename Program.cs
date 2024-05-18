15fab4c9bb14e30db8a032e937389e4f342f7844
Leslie Farago <leslie.farago@phri.ca>
2024-05-18 00:30:17 -0400

    Added an htmx form post that saves the person's name to an xml file.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-app.MapGet("/messages", async (context) => await context.Response.WriteAsync($"Click me again to trigger another GET request and watch the time change. {DateTime.UtcNow}")); 
+app.MapGet("/messages", async (context) => await context.Response.WriteAsync($"Click me again to trigger another GET request and watch the time change. {DateTime.UtcNow}"));
+app.MapPost("/setup/yourname", async (context) =>
+{
+    // Read the name entered by the user
+    var formData = await context.Request.ReadFormAsync();
+    var name = formData["Name"].ToString();
+
+    // Serialize the name to an XML file
+    var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(string));
+    using (var writer = new System.IO.StreamWriter($"Data/{name}.xml"))
+    {
+        xmlSerializer.Serialize(writer, name);
+    }
+
+    // Return 200 OK
+    context.Response.StatusCode = 200;
+});
 
 app.UseStaticFiles();
 app.MapRazorPages();
