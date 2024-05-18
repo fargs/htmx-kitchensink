3d7cff0aa56021c5cc48133b510a753b2931ec67
Leslie Farago <leslie.farago@phri.ca>
2024-05-17 23:30:14 -0400

    Added my first element that uses htmx. A clickable div.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.MapGet("/messages", async (context) => await context.Response.WriteAsync($"Click me again to trigger another GET request and watch the time change. {DateTime.UtcNow}")); 
+
 app.UseStaticFiles();
 app.MapRazorPages();
 app.Run();
